9e67331fa8988268011c391f22d519d3b6222b6c
Ashutosh2002 <ashutoshswarup2002@gmail.com>
2021-07-19 18:50:56 +0530

    Finished Frontend

---
+++ src/users/pages/Users.js
@@ -0,0 +1,48 @@
+import React, { useEffect, useState } from "react";
+
+import UsersList from "../components/UsersList";
+import ErrorModal from "../../shared/components/UIElements/ErrorModal";
+import LoadingSpinner from "../../shared/components/UIElements/LoadingSpinner";
+import { useHttpClient } from "../../shared/hooks/http-hook";
+
+const Users = () => {
+  // const USERS = [
+  //   {
+  //     id: "u1",
+  //     name: "Ashutosh",
+  //     image:
+  //       "https://c.ndtvimg.com/2021-06/7jhlima_virat-kohli-wtc-final-afp_625x300_24_June_21.jpg?im=FaceCrop,algorithm=dnn,width=806,height=605",
+  //     places: 3,
+  //   },
+  // ];
+
+  const { isLoading, error, sendRequest, clearError } = useHttpClient();
+  const [loadedUsers, setLoadedUsers] = useState();
+
+  useEffect(() => {
+    const fetchUsers = async () => {
+      try {
+        const responseData = await sendRequest(
+          process.env.REACT_APP_BACKEND_URL + "/users"
+        );
+
+        setLoadedUsers(responseData.users);
+      } catch (err) {}
+    };
+    fetchUsers();
+  }, [sendRequest]);
+
+  return (
+    <React.Fragment>
+      <ErrorModal error={error} onClear={clearError} />
+      {isLoading && (
+        <div className="center">
+          <LoadingSpinner />
+        </div>
+      )}
+      {!isLoading && loadedUsers && <UsersList items={loadedUsers} />}
+    </React.Fragment>
+  );
+};
+
+export default Users;
